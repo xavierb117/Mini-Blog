d1369e58a0fd365e865aa4e9be3eb3b7c80eb66c
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-02-27 14:47:30 -0800

    added styling

--- app.js
+++ app.js
@@ -14,21 +14,7 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.post('/thankyou', (req, res) =>
-{
-    console.log(req.body);
 
-    const order = 
-    {
-        fname: req.body.fname,
-        email: req.body.email,
-        flavor: req.body.flavor,
-        method: req.body.method,
-        userInput: req.body.userInput
-    };
-
-    res.render('thank-you', {order});
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
