ed03d4fdecf08123adbaffca3c44f8144a3b99b1
xavierb <194048107+xavierb117@users.noreply.github.com>
2025-03-02 23:39:29 -0800

    Added a confirmation view

--- app.js
+++ app.js
@@ -14,8 +14,19 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.post('/submit', (req, res) => {
+    const newPost = {
+        author: req.body.author,
+        title: req.body.title,
+        content: req.body.content
+    };
 
+    console.log(newPost)
+
+    res.render('confirmation', {newPost});
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
 })
+
