8d6397c2039fdd8862b3a5114af8a6176a42b16a
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-03-04 14:55:49 -0800

    added part of client side validation

--- public/scripts/client-validation.js
+++ public/scripts/client-validation.js
@@ -1,8 +1,16 @@
 document.getElementById('blog-form').onsubmit = () => {
 
     // clearErrors();
+    let isValid = true;
 
-    
+    // Validate Author - must be there and have no nums
+    const author = document.getElementById('author').value.trim();
+    if (!author) {
+        document.getElementById('err-author').style.display = 'inline';
+        isValid = false;
+    }
+
+    return isValid;
 
 };
 
