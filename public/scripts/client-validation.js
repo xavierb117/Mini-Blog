539f10f8a81292f29c54c0256fde7fdfd8b102e7
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-03-11 13:09:40 -0700

    finished front end validation

--- public/scripts/client-validation.js
+++ public/scripts/client-validation.js
@@ -1,12 +1,30 @@
 document.getElementById('blog-form').onsubmit = () => {
 
-    // clearErrors();
+    clearErrors();
     let isValid = true;
 
-    // Validate Author - must be there and have no nums
+    // Validate Author - must have input and have no nums
     const author = document.getElementById('author').value.trim();
     if (!author) {
         document.getElementById('err-author').style.display = 'inline';
+        isValid = false;
+    }
+    if (hasNumber(author)) {
+        document.getElementById('err-author').style.display = 'inline';
+        isValid = false;
+    }
+
+    // Validate Title - must have input
+    const title = document.getElementById('title').value.trim();
+    if (!title) {
+        document.getElementById('err-title').style.display = 'inline';
+        isValid = false;
+    }
+
+    // Validate content - must have atleast 10 characters
+    const content = document.getElementById('content').value.trim();
+    if (content.length < 10) {
+        document.getElementById('err-content').style.display = 'inline';
         isValid = false;
     }
 
@@ -19,4 +37,14 @@ function clearErrors() {
     for (let err of errors) {
         err.style.display = "none";
     }
+}
+
+function hasNumber(author) {
+    for (let i = 0; i < author.length; i++) {
+        let char = author.charAt(i);
+        if (!isNaN(char)) {
+            return true;
+        }
+    }
+    return false
 }
